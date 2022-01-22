4aee8eab697207792fea5c925bc5d39dd1d9bd83
Chris Hatch <seige.hatch@gmail.com>
2022-01-22 09:46:40 -0600

    Profiling set up

--- src/get_primes/core.cljs
+++ src/get_primes/core.cljs
@@ -1,13 +1,18 @@
-(ns get-primes.core)
+(ns get-primes.core ; ClojureScript namespace
+  (:require [taoensso.tufte :as tufte :refer-macros (defnp p profiled profile)]))
 
-(defn isPrime [n]
-(let [rootN (Math/floor (Math/sqrt n))]
-(reduce (fn [bool, m] (if (= (mod n m) 0) (reduced false) true))
-  true (range 2 rootN))))
+;; We'll request to send `profile` stats to `println`:
+(tufte/add-basic-println-handler! {})
+
+(defn isPrime [n] (let [rootN (Math/floor (Math/sqrt n))]
+  (reduce (fn [bool, m]
+    (if (= (mod n m) 0) (reduced false) true))
+    true (range 2 rootN))))
 
 (defn getPrimes [start end]
-  (reduce (fn [primeList, n]
-      (-> n isPrime (#(if % (conj primeList n) primeList))))
-    () (range start end)))
+  (p ::prime-test
+    (reduce (fn [primeList, n]
+        (-> n isPrime (#(if % (conj primeList n) primeList))))
+    () (range start end))))
 
-(time (println (count (getPrimes 2 1e6)) "primes found"))
+(time (println (count (profile {} (getPrimes 2 1e6))) "primes found"))
