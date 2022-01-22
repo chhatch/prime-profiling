548efb1264114d4d97461bfc2937220da91b406a
Chris Hatch <seige.hatch@gmail.com>
2022-01-22 11:52:47 -0600

    Useful profiling, poor readability

--- src/get_primes/core.cljs
+++ src/get_primes/core.cljs
@@ -4,15 +4,16 @@
 ;; We'll request to send `profile` stats to `println`:
 (tufte/add-basic-println-handler! {})
 
-(defn isPrime [n] (let [rootN (Math/floor (Math/sqrt n))]
-  (reduce (fn [bool, m]
-    (if (= (mod n m) 0) (reduced false) true))
-    true (range 2 rootN))))
+(defn isPrime [n] (p ::isP
+ (let [rootN  (p ::isP_calc-root (Math/floor (Math/sqrt n)))]
+    (p ::isP_reduce (reduce (fn [bool, m]
+      (p ::isP_reduce_fn (if (p ::isP_reduce_fn_check-mod-0 (= (mod n m) 0)) (p ::isP_reduce_fn_mod-0 (reduced false)) (p ::isP_reduce_fn_not-mod-0 true))))
+      true (p ::isP_reduce_create-range (range 2 rootN)))))))
 
 (defn getPrimes [start end]
-  (p ::prime-test
+  (p ::getPrimes
     (reduce (fn [primeList, n]
         (-> n isPrime (#(if % (conj primeList n) primeList))))
     () (range start end))))
 
-(time (println (count (profile {} (getPrimes 2 1e6))) "primes found"))
+(time (println (count (profile {} (getPrimes 2 1e7))) "primes found"))
