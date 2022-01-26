97082208694877a2d354f19837602589a943eab8
Chris Hatch <chris.hatch@hotelengine.com>
2022-01-26 10:11:21 -0600

    Things are looking cleaner

--- src/get_primes/core.cljs
+++ src/get_primes/core.cljs
@@ -1,21 +1,27 @@
 (ns get-primes.core ; ClojureScript namespace
-  (:require [taoensso.tufte :as tufte :refer-macros (defnp p profiled profile)]))
+  ;(:require [taoensso.tufte :as tufte :refer-macros (defnp p profiled profile)]))
+)
+;(tufte/add-basic-println-handler! {})
 
-;; We'll request to send `profile` stats to `println`:
-(tufte/add-basic-println-handler! {})
+(defn isRelativePrime [n m]
+  (not= (mod n m) 0))
 
-(defn isPrime [n, rootN]
-  (loop [m 2 relativePrime (not= (mod n 2) 0) continue (< 2 rootN)]
-    (if continue
-      (if relativePrime
-        (recur (inc m) (not= (mod n (inc m)) 0) (< (inc m) rootN))
-        false)
-      relativePrime)))
+(defn shouldContinue [m rootN]
+  (< m rootN))
+
+(defn isPrime [n]
+  (let [rootN (int (Math/sqrt n))]
+    (loop [m 2]
+      (if (shouldContinue m rootN)
+        (if (isRelativePrime n m)
+          (recur (inc m))
+          false)
+        relativePrime))))
 
 (defn getPrimes [start end]
     (loop [n start primeList ()]
         (if (> n end) primeList
-          (recur (inc n) (if (isPrime n (Math/floor (Math/sqrt n))) (conj primeList n)
+          (recur (inc n) (if (isPrime n) (conj primeList n)
           primeList)))))
 
-(time (println (count (profile {} (getPrimes 2 1e4))) "primes found"))
+(time (println (count (getPrimes 2 1e6)) "primes found"))
